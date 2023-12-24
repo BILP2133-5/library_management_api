0db14ab1664f72bb8fa2784fcb91933ac42d843b
Ali Rıza Şahin <arsahin.dev@gmail.com>
2023-12-25 01:21:38 +0300

    - create user data access layer - create protectedRoute service layer function - refactor overall error handling of the auth domain

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,18 +1,23 @@
 import { Request, Response } from "express";
-import User from "../models/user.model";
-import * as authService from "../services/auth.service";
+import * as AuthService from "../services/auth.service";
 
 export const register = async (req: Request, res: Response): Promise<void> => {
   try {
     const newUser = req.body;
-    const token = await authService.register(newUser);
+    const token = await AuthService.register(newUser);
 
     res.status(200).json({ token });
   } catch (error) {
     if (error instanceof Error) {
-      res.status(400).json({ error: error.message });
-    } else {
-      res.status(500).json({ error: 'Unknown Error' });
+      if (error.cause === "passwordHashing") {
+        res.status(500).json({ error: "Internal server error while password hashing." });
+      } else if (error.cause === "userDocumentCreation") {
+        res.status(500).json({ error: "Internal server error while trying to create a new user document." })
+      } else if (error.cause === "jwtTokenSigning") {
+        res.status(500).json({ error: "Internal server error while trying to sign the JWT token." })
+      } else {
+        res.status(500).json({ error: error.message }); 
+      }
     }
   }
 };
@@ -20,14 +25,16 @@ export const register = async (req: Request, res: Response): Promise<void> => {
 export const login = async (req: Request, res: Response): Promise<void> => {
   try {
     const { email, password } = req.body;
-    const token = await authService.login(email, password);
+    const token = await AuthService.login(email, password);
     res.status(200).json({ token });
   } catch (error) {
     if (error instanceof Error) {
-      if (error.message === 'User not found' || error.message === 'Invalid password') {
-        res.status(401).json({ error: error.message });
+      if (error.cause === "emptyQueryResult") {
+        res.status(404).json({ error: "There's no such user." });
+      } else if (error.cause === 'incorrectPassword') {
+        res.status(401).json({ error: "Given password is incorrect." });
       } else {
-        res.status(500).json({ error: 'Unknown Error' });
+        res.status(500).json({ error: error.message });
       }
     }
   }
@@ -54,15 +61,16 @@ export const protectedRoute = async (req: Request, res: Response) => {
   const userId = (req as any).user.userId; 
 
   try {
-    const user = await User.findById(userId);
-
-    if (!user) {
-      res.status(404).json({ error: 'User not found' });
-      return;
-    }
+    const user = await AuthService.protectedRoute(userId);
 
     res.status(200).json({ user });
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    if (error instanceof Error) {
+      if (error.cause === "emptyQueryResult") {
+        res.status(404).json({ error: 'User not found' });
+      } else {
+        res.status(500).json({ error: error.message });
+      }
+    }
   }
 };
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,34 +1,37 @@
-import { IUser } from '../models/user.model';
-import User from '../models/user.model';
 import bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 
-const secretkey = process.env.JWT_SECRET as string;
+import * as UserDataAccess from '../data-access/user';
 
-interface NewUser {
-  name: string;
-  email: string;
-  password: string;
-  role?: "user" | "admin" | "superadmin";
-}
-export async function register(newUser: NewUser): Promise<string> {
+const secretKey = process.env.JWT_SECRET as string;
+
+export async function register(newUser: UserDataAccess.NewUser): Promise<string> {
   try {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(newUser.password, saltRounds);
-
     newUser.password = hashedPassword;
+  } catch (error) {
+    throw new Error("Error while password hashing.", { cause: "passwordHashing" })
+  }
 
-    const user = new User(newUser);
-    await user.save();
 
-    const token = jwt.sign({ userId: user._id, role: user.role }, secretkey, {
+  let createdUserDocument;
+  try {
+    createdUserDocument = await UserDataAccess.createUser(newUser);
+  } catch (error) {
+    throw new Error("Couldn't create new user document on database.", { cause: "userDocumentCreation" })
+  }
+
+  let token;
+  try {
+    token = jwt.sign({ userId: createdUserDocument._id, role: createdUserDocument.role }, secretKey, {
       expiresIn: '1h', // Token expiration time
     });
-
-    return token;
   } catch (error) {
-    throw new Error('Registration failed');
+    throw new Error("Problem while signing the userId role.", { cause: "jwtTokenSigning" })
   }
+
+  return token;
 }
 
 export async function createAdmin(newUser: IUser): Promise<string> {
@@ -53,22 +56,28 @@ export async function createAdmin(newUser: IUser): Promise<string> {
 }
 
 export async function login(email: string, password: string): Promise<string> {
-  const user = await User.findOne({ email });
-
+  const user = await UserDataAccess.getUserByEmail(email);
   if (!user) {
-    throw new Error('User not found');
+    throw new Error('User not found.', { cause: "emptyQueryResult" });
   }
 
   const isPasswordValid = await bcrypt.compare(password, user.password);
-
   if (!isPasswordValid) {
-    throw new Error('Invalid password');
+    throw new Error('Incorrect password.', { cause: "incorrectPassword" });
   }
 
-  const token = jwt.sign({ userId: user._id, role: user.role }, secretkey, {
+  const token = jwt.sign({ userId: user._id, role: user.role }, secretKey, {
     expiresIn: '1h', // Token expiration time
   });
 
   return token;
 }
 
+export async function protectedRoute(userId: string) {
+  const user = await UserDataAccess.getUserById(userId);
+  if (!user) {
+    throw new Error("No such user is found.", { cause: "emptyQueryResult" })
+  }
+
+  return user;
+}
