e2bce1dfd50b8b18623b35e9b11c715c9224f499
Ali Rıza Şahin <arsahin.dev@gmail.com>
2023-12-25 00:41:59 +0300

    remove unnecessary implicit type annotations of auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
-import User,{ IUser } from "../models/user.model";
+import User from "../models/user.model";
 import * as authService from "../services/auth.service";
 
 export const register = async (req: Request, res: Response): Promise<void> => {
   try {
-    const newUser: IUser = req.body;
+    const newUser = req.body;
     const token = await authService.register(newUser);
+
     res.status(200).json({ token });
-  } catch (error: any) {
+  } catch (error) {
     if (error instanceof Error) {
       res.status(400).json({ error: error.message });
     } else {
@@ -21,7 +22,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     const { email, password } = req.body;
     const token = await authService.login(email, password);
     res.status(200).json({ token });
-  } catch (error: any) {
+  } catch (error) {
     if (error instanceof Error) {
       if (error.message === 'User not found' || error.message === 'Invalid password') {
         res.status(401).json({ error: error.message });
@@ -62,7 +63,6 @@ export const protectedRoute = async (req: Request, res: Response) => {
 
     res.status(200).json({ user });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
