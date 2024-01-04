a0972d6be59ce0d086ab563d7a079c9f7bed2549
Ali Rıza Şahin <arsahin.dev@gmail.com>
2024-01-04 22:54:59 +0300

    add unloan route

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+
 import * as BookController from '../controllers/book.controller';
+import { authorize } from '../middleware/authMiddleware';
 
 const bookRouter = express.Router();
 
@@ -10,6 +12,7 @@ bookRouter.get('/search/:query', BookController.searchBooks);
 bookRouter.put('/update/:id', BookController.updateBook);
 bookRouter.post('/add', BookController.addBook);
 bookRouter.post('/loan/:bookId', BookController.loanBook);
+bookRouter.post('/unloan/:bookId', authorize(["admin", "superadmin"]), BookController.loanBook);
 
 bookRouter.delete('/remove/:id', BookController.removeBookById);
 
